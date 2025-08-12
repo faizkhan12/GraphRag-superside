2accb93ec1bd6fc7d9fa438c3c3998f8354db68e
faizkhan12 <faizk6797@gmail.com>
2025-08-12 16:34:06 +0530

    add wsgi for gunicorn server and remove proxy server

---
+++ backend/wsgi.py
@@ -0,0 +1,11 @@
+"""
+WSGI file for the Gunicorn server.
+"""
+
+from app.web import create_app
+
+# Create the Flask app instance
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8000)
